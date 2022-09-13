e7d413cea49e6841a91611c3e45392f154d3067f
kimnamhyeon <knh306@naver.com>
2022-09-13 18:37:49 +0900

    Chapter_01_컴퓨터 프로그래밍에 대한 소개

---
+++ Chapter01/Chapter01.c
@@ -0,0 +1,11 @@
+//program 1.1
+#include <stdio.h>
+
+int main()
+{
+	float c, pi = 3.1416, r = 2.0;		//pi가 실수이므로 변수를 모두 실수로 지정
+	c = 2.0 * pi * r;					//원의 둘레=2 * pi * r
+	printf("원의 둘레는 %f 입니다", c);
+
+	return 0;
+}
